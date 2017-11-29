4f514be3f931b998985bdb427ea15635bf41cb2c
Nikola <zileslayer@gmail.com>
2017-11-29 21:30:01 +0100

    crest

--- navMenu.php
+++ navMenu.php
@@ -1,24 +1,3 @@
-<<<<<<< HEAD:navMenu.php
-<nav class="navbar navbar-default">
-	<div class="container-fluid">
-		<div class="navbar-header">
-			<a class="navbar-brand" href="index.php">Dashboard</a>
-		</div>
-		<ul class="nav navbar-nav">
-			<li><a href="member.php">Members</a></li>
-			<li><a href="customer.php">Customers</a></li>
-			<?php
-			if ((isset ($_SESSION ['id'])) && ($_SESSION ['user'] == "admin")) {
-				?>
-				<li><a href="menu.php">Menu</a></li>
-				<li><a href="driver.php">Drivers</a></li>
-				<li><a href="inventory.php">Inventories</a></li>
-				<li><a href="generate.php">Export</a></li><?php } ?>
-			<li><a href="functions/logout.php">Log out</a></li>
-		</ul>
-	</div>
-</nav>
-=======
 <nav class="navbar navbar-default">
 	<div class="container-fluid">
 		<div class="navbar-header">
@@ -38,4 +17,3 @@
 		</ul>
 	</div>
 </nav>
->>>>>>> 0de87ecc53637820116db3342b8a3fdc0f47809d:nav_menu.php
